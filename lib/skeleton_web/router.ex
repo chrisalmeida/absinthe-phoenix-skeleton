d78408b03095a389fd385764011c153cf9293447
Chris Almeida <run.revelations@gmail.com>
2018-11-20 14:41:30 -0800

    verify jwt in plug pipeline

--- lib/skeleton_web/router.ex
+++ lib/skeleton_web/router.ex
@@ -1,8 +1,10 @@
 defmodule SkeletonWeb.Router do
   use SkeletonWeb, :router
+  require Logger
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :verify_jwt
   end
 
   scope "/api" do
@@ -11,5 +13,29 @@ defmodule SkeletonWeb.Router do
     forward "/", Absinthe.Plug,
       schema: SkeletonWeb.GraphQL.Schema,
       json_codec: Jason
+  end
+
+  @doc """
+    Verify that signed JWT was sent with response
+    and that there is a user with an ID in JWT
+  """
+  defp verify_jwt(conn, _) do
+    with ["Bearer: " <> auth] <- Plug.Conn.get_req_header(conn, "authorization"),
+         {:ok, %{"user" => %{"id" => id} = user}} <- Skeleton.Token.verify_and_validate(auth),
+         false <- is_nil(id) do
+      put_private(conn, :absinthe, %{context: user})
+    else
+      e ->
+        Logger.error(fn ->
+          h = Plug.Conn.get_req_header(conn, "authorization")
+
+          "ip: #{conn.host} path: #{conn.request_path} error: #{inspect(e)} authorization: #{
+            inspect(h)
+          }"
+        end)
+
+        Plug.Conn.send_resp(conn, 403, Jason.encode!(%{error: "unauthorized"}))
+        |> Plug.Conn.halt()
+    end
   end
 end
